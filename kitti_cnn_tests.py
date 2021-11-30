8526784d027cffb9cfb3aebffdbd5b08eb8ee947
Peyton Johnson <35665039+willjohnsonk@users.noreply.github.com>
2021-11-30 01:10:50 -0600

    Improved dataframe for examples

--- kitti_cnn_tests.py
+++ kitti_cnn_tests.py
@@ -59,9 +59,42 @@ for img in test_imgs:
 
 tests = np.expand_dims(tests, axis=-1)
 
+
+img_name = []
+
 for i in range(len(tests)):
+    img_name.append(names[i])
     prediction = model.predict(tests[i])
     label_guess.append(np.argmax(prediction[0]))
     label_conf.append(100 * (np.amax(prediction)))
 
-    print("Guessed: ", np.argmax(prediction[0]), names[i])
+label_dict = {0:'Car',1:'Van',2:'Truck',3:'Pedestrian',4:'Person_sitting',5:'Cyclist',6:'Tram'}
+
+id_dict = {
+    '000000.png':'Pedestrian', '000001.png':'Car', '000002.png':'Cyclist', '000003.png':'Pedestrian',
+    '000004.png':'Car', '000005.png':'Car', '000006.png':'Truck', '000007.png':'Car',
+    '000008.png':'Van', '000009.png':'Truck', '000010.png':'Van', '000011.png':'Tram',
+   '000012.png':'Pedestrian', '000013.png':'Cyclist', '000014.png':'Cyclist', '000015.png':'Truck',
+   '000016.png':'Cyclist', '000017.png':'Car', '000018.png':'Car', '000019.png':'Tram'
+}
+
+guessed = []
+expected = []
+
+for label in label_guess:
+    guessed.append(label_dict[label])
+
+for exp in img_name:
+    expected.append(id_dict[exp])
+
+test_data = {
+    "Image ID": img_name,
+    "Expected": expected,
+    "Guessed": guessed,
+    "Confidence": label_conf
+}
+
+test_frame = pd.DataFrame(test_data)
+test_frame.sort_values(by=['Confidence'], inplace=True, ascending=False)
+
+print(test_frame)
